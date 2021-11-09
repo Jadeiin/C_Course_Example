0ad3bec8e7c9a1d3e8cab95bfddae68f1ef7b35b
Jadeiin <github@qunn.eu>
2021-11-09 15:35:52 +0800

    update

--- Lexue/42.c
+++ Lexue/42.c
@@ -2,24 +2,51 @@
 #include <ctype.h>
 int main(void)
 {
-    int max=0,min=0,num,res;
+    int max=0,min=0,num=0,res,flag=0,flag2=0,flag3=0;
     char c,pun;
     while((c=getchar())!='\n')
     {
-        if(isdigit(c))
+        if(isdigit(c)&&flag==0)
         {
-            num=(int)(c-'0');
-            if(num>max)max=num;
-            if(num<min)min=num;
+        	num*=10;
+            num+=(int)(c-'0');
         }
-        if(c=='+')pun=c,res=max+min;
-        else if(c=='-')pun=c,res=max-min;
-        else if(c=='*')pun=c,res=max*min;
-        else if(c=='/')pun=c,res=max/min;
-        else if(c=='%')pun=c,res=max%min;
+        else if(isdigit(c)&&flag==1)
+        {
+        	num*=10;
+            num-=(int)(c-'0');
+            flag2=0;
+        }
+        if(c==','&&flag3==1)
+        {
+            if(flag2==1){pun='-';break;}
+        	if(num>max)max=num;
+            if(num<min)min=num;
+            num=0;
+            flag=0;
+		}
+		else if(c==','&&flag3==0)
+		{
+			max=num;
+			min=num;
+            num=0;
+            if(flag2==1)pun='-';
+            flag=0;
+            flag3=1;
+		}
+        if(c=='+'){pun=c;break;}
+        else if(c=='-')flag=1,flag2=1;
+        else if(c=='*'){pun=c;break;}
+        else if(c=='/'){pun=c;break;}
+        else if(c=='%'){pun=c;break;}
     }
-    if(min>0&&max>0)printf("%d %c %d = %d\n",max,pun,min,res);
-    else if(min<0&&max>0)printf("%d %c (%d) = %d\n",max,pun,min,res);
-    else if(min<0&&max<0)printf("(%d) %c (%d) = %d\n",max,pun,min,res);
+    if(pun=='+')res=max+min;
+    else if(pun=='-')res=max-min;
+    else if(pun=='*')res=max*min;
+    else if(pun=='/')res=max/min;
+    else if(pun=='%')res=max%min;
+    if(pun=='+'||pun=='-'||pun=='*'||pun=='/'||pun=='%'){if(min>0)printf("%d %c %d = %d\n",max,pun,min,res);
+    else if(max<0)printf("(%d) %c (%d) = %d\n",max,pun,min,res);
+    else printf("%d %c (%d) = %d\n",max,pun,min,res);}
     return 0;
 }
--- Lexue/43.c
+++ Lexue/43.c
@@ -1,94 +1,47 @@
-#include <stdio.h>
-#include <string.h>
-int date(long x,char a[][25])
+#include<stdio.h>
+#include<string.h>
+static int w[17]={7,9,10,5,8,4,2,1,6,3,7,9,10,5,8,4,2};
+static char m[11]="10X98765432";
+void check18(char num[])
 {
-	int year=(int)(a[x][6]-'0')*1000+(int)(a[x][7]-'0')*100+(int)(a[x][8]-'0')*10+(int)(a[x][9]-'0');
-	int month=(int)(a[x][10]-'0')*10+(int)(a[x][11]-'0');
-	int day=(int)(a[x][12]-'0')*10+(int)(a[x][13]-'0');
-	int back=0;
-	if((year%4==0&&year%100!=0)||year%400==0)
+	int i,S=0,Y;
+	for(i=0;i<17;i++)
+	   S+=(num[i]-'0')*w[i];
+	Y=S%11;
+	if(m[Y]==num[17])
+	  printf("Valid\n");
+	else printf("Invalid\n");
+}
+void check(char num[])
+{
+	int i;
+	i=strlen(num);
+	if(i!=15)
 	{
-		switch(month)
-		{
-			case 1:
-			case 3:
-			case 5:
-			case 7:
-			case 8:
-			case 10:
-			case 12:{if(day<1||day>31)back=0;else back=1;break;}
-			case 4:
-			case 6:
-			case 9:
-			case 11:{if(day<1||day>30)back=0;else back=1;break;}
-			case 2:{if(day<1||day>29)return 0;else return 1;break;}
-		}
-		return back;
+		if(i==18) check18(num);
+		else printf("Invalid\n");
 	}
 	else
 	{
-		switch(month)
-		{
-			case 1:
-			case 3:
-			case 5:
-			case 7:
-			case 8:
-			case 10:
-			case 12:{if(day<1||day>31)back=0;else back=1;break;}
-			case 4:
-			case 6:
-			case 9:
-			case 11:{if(day<1||day>30)back=0;else back=1;break;}
-			case 2:{if(day<1||day>28)return 0;else return 1;break;}
-		}
-		return back;
-	}
+		char num1[18]={num[0],num[1],num[2],num[3],num[4],num[5],'1','9',num[6],num[7],num[8],num[9],num[10],num[11],num[12],num[13],num[14],0};//这段字符串处理写的不好
+		int S=0;
+		if (num[12]=='9'&&num[13]=='9'&&num[14]>='6'&&num[14]<='9')
+		   	num1[7]='8';
+		for(i=0;i<17;i++)
+		  S+=(num1[i]-'0')*w[i];
+		num1[17]=m[S%11];
+		puts(num1);
+    }
 }
 int main(void)
 {
-	long n;
-	int x,y,S,Y,i,flag;
-	scanf("%ld\n",&n);
-	const long N=n;
-	char a[N][25];
-	char b[11]={"10X98765432"};
-	int c[17]={7,9,10,5,8,4,2,1,6,3,7,9,10,5,8,4,2};
-	for(x=0;x<n;++x)fgets(a[x],25,stdin);
-	for(x=0;x<n;++x)
-	{
-		flag=0;
-		for(i=0;i<15;++i)if(a[x][i]>'9'||a[x][i]<'0')flag=1;
-		if(flag==1){printf("Invalid\n");continue;}
-		if(strlen(a[x])==19)
-		{
-			for(i=0,S=0;i<17;++i)S+=(int)(a[x][i]-'0')*c[i];
-   			Y=S%11;
-			if(b[Y]==a[x][17]&&date(x,a))printf("Valid\n");else printf("Invalid\n");
-   		}
-		else if(strlen(a[x])==16&&a[x][12]=='9'&&a[x][13]=='9'&&a[x][14]>'5'&&a[x][14]<='9')
-   			{
-   			for(y=16;y>7;--y) a[x][y]=a[x][y-2];
-   			a[x][6]='1';
-   			a[x][7]='8';
-   			for(i=0,S=0;i<17;++i)S+=(int)(a[x][i]-'0')*c[i];
-   			Y=S%11;
-   			a[x][17]=b[Y];
-   			a[x][18]='\n';
-   			printf("%s",a[x]);
-   			}
-   		else if(strlen(a[x])==16)
-    	{
-   		for(y=16;y>7;--y)a[x][y]=a[x][y-2];
-  	 	a[x][6]='1';
-   		a[x][7]='9';
-		for(i=0,S=0;i<17;++i)S+=(int)(a[x][i]-'0')*c[i];
-		Y=S%11;
-   		a[x][17]=b[Y];
-   		a[x][18]='\n';
-   		printf("%s",a[x]);
-   		}
-		else printf("Invalid\n");
-	}
-   	return 0;
+	int n,i;
+	scanf("%d",&n);
+	getchar();
+	char num[n][22];
+	for(i=0;i<n;i++)
+	   gets(num[i]);
+	for(i=0;i<n;i++)
+	   check(num[i]);
+	return 0;
 }
