4bfbeee59961f25957e1ae41f255193507a2b5dd
Jadeiin <github@qunn.eu>
2021-11-19 19:22:57 +0800

    update

--- Lexue/51.c
+++ Lexue/51.c
@@ -1,19 +1,8 @@
 #include <stdio.h>
-unsigned long long int count=0;
-int judge(int a,int b)
-{
-   if(a==b)
-   {
-      count++;
-      return 0;
-   }
-   else if(a+1==b)return judge(a+1,b);
-   else return judge(a+1,b)+judge(a+2,b);
-}
 int main() {
    int a,b;
    scanf("%d %d",&a,&b);
-   judge(a,b);
+   for
    printf("%llu\n",count);
    return 0;
 }
